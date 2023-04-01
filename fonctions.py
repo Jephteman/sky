39690651382bb78441ab848311005beb99057086
Jephte Mangenda <Jephte0Mangenda@gmail.com>
2023-04-01 19:30:18 +0000

    Initiale commit
    Hi

---
+++ fonctions.py
@@ -0,0 +1,118 @@
+from colorama import init, Fore
+from rich.console import Console
+from rich.table import Table
+from shodan.client import Shodan
+import subprocess
+import requests
+
+init(autoreset=True)
+console = Console()
+
+
+Instance = ''
+
+
+def init(key):
+	global Instance 
+	Instance = Shodan(key)
+
+def verif_key():
+	i = subprocess.getstatusoutput('cat ~/.config/shodan/api_key')
+	if i[0] == 0:
+		return i[1]
+	else:
+		return False
+
+def banner_1():
+	print("""
+	 ________________________________________________________________
+	/                                                                \
+	|  Welcom to sky , a simple promgram to hack the world machine , |
+	\                                                                /
+	 ----------------------------------------------------------------\
+	    \
+	     \
+	         .--.
+	        |o_o |
+	        |:_/ |
+	       //   \ \
+	      (|     | )
+	     /'\_   _/`\
+	     \___)=(___/
+
+		""")
+
+def vuln(ip:str):
+	"""
+		show vuln of ip
+	"""
+	x = requests.get(f"https://internetdb.shodan.io/{ip}").json()
+	if x['detail'] == 'No information available':
+		print('Information indisponible')
+		return
+	
+	return x
+
+def info(ip:str,**argv):
+	table = Table(title=f'Infoarmation to {ip} ')
+	"""
+		Gie information about ip
+	"""
+	try:
+		x = Instance.host(ip)
+	except shodan.exception.APIError:
+		print(Fore.RED + '[+] Aucunne Information sur cette adresse IP ')
+		return
+	return x
+	
+
+def filters(resultat,**argv):
+	if len(argv.keys()) == 0:
+		return resultat
+
+	condition = 'ret = True if '
+	n = 0
+	for i in argv.keys():
+		if n == 1:
+			condition +=  "'{resultat}['{i}']' == '{argv}['{i}']' ".format(i=i)
+		else:
+			condition +=  "and '{resultat}['{i}']' == '{argv}['{i}']' ".format(i=i)
+			n +=1
+	
+	condition += 'else True'
+
+	for i in resultat['matches']:
+		exec(condition.format(argv=argv,resultat=i))
+		if ret:
+			y.append(i)
+
+
+def seach_service(kind,**argv):
+	support_kinds = {'adb':'Android Debug Bridge','vnc':'vnc',\
+	'vmware':'vmware','docker':'docker','couchdb':'couchdb',\
+	'screenshot':'has_screenshot:true','ganglia':'product:"Ganglia XML Grid Monitor"',\
+	'bitcoin':'satoshi','chromecast':'chromecast','cloud': ['Amazon cloud','cloud Azure','cloud Google'],\
+	'cockroachdb':'cockroachdb','dns':['dns-tcp','dns-udp recursion enabled','dns-udp recursion enabled'],\
+	'ftp':['ftp','ftp 220']}
+
+	if kind not in support_kinds.keys():
+		print(f'{kind} is not supported')
+		return 
+
+	x = Instance.search(kind)
+	return filters(x,argv)
+
+	
+
+
+	
+
+
+	
+
+	
+
+
+
+
+
---
+++ interative.py
@@ -0,0 +1,69 @@
+from rich.console import Console
+from rich.table import Table
+from fonctions import *
+
+console = Console()
+table = Table()
+
+def view_vuln(ip:str):
+    x=vuln(ip)
+
+    table.add_column("Type", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Value", style="magenta")
+    table.add_row('hostnames',x['hostnames'])
+    table.add_row('ports',x['ports'])
+    table.add_row('vulns',x['vulns'])
+    console.print(table)
+
+def view_info(ip:str,**argx):
+    x = info(ip)
+    
+    table.add_column("Type", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Value", style="magenta")
+
+    table.add_row('ip',x['ip_str'])
+
+    if 'asn' in x.keys():
+    	table.add_row('asn',x['asn'])
+    if 'ports' in x.keys():
+    	table.add_row('ports',str(x['ports']))
+    if 'vulns' in x.keys():
+    	table.add_row('vuls',x['vulns'])
+    table.add_row('os',x['os'])
+
+    if 'domains' in x.keys():
+    	table.add_row('domains',str(x['domains']))
+    if 'org' in x.keys():
+    	table.add_row('org',x['org'])
+    if 'hostnames' in x.keys():
+    	table.add_row('hostnames',x['hostnames'])
+    if 'location' in x.keys():
+    	table.add_row('country_name',x['location']['country_name'])
+    	table.add_row('city',x['location']['city'])
+    	table.add_row('longitude',x['location']['longitude'])
+    	table.add_row('latitude',x['location']['latitude'])
+    if 'data' in x.keys():
+        table.add_column('Data',x['data'])
+    console.print(table)
+
+def search_peer_kinds(kind:str,**argv):
+    x = seach_service(kind,argv)
+
+    table.add_column("Type", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Value", style="magenta")
+
+    for i in x:
+        table.add_row('IP',i['ip_str'])
+        table.add_row('Ports',i['ports'])
+        table.add_row('Location',i['location'])
+        console.print(table)
+
+def start():
+    key = verif_key()
+    if not key :
+        init(input(('Please enter your shodan api key :')))
+    else:
+        init(key)
+
+    
+
